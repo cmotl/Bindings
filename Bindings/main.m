1334382bf3046b1182acdcf68d237a1900bc1528
Christopher Motl <cmotl@cmotl.com>
2011-12-04 00:13:22 -0600

    Initial Commit

--- Bindings/main.m
+++ Bindings/main.m
@@ -7,14 +7,27 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+#import "Person.h"
 
 int main (int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Person *p1 = [Person new];
+        Person *p2 = [Person new];
+        
+        [p1 bind:@"name" toObject:p2 withKeyPath:@"name" options:nil];
+        [p2 bind:@"name" toObject:p1 withKeyPath:@"name" options:nil];
+        
+        p1.name = @"Christopher";
+        NSLog(@"p1 name: %@", p1.name);
+        NSLog(@"p2 name: %@", p2.name);
+        
+        p2.name = @"Christina";
+        NSLog(@"p1 name: %@", p1.name);
+        NSLog(@"p2 name: %@", p2.name);
         
     }
     return 0;
